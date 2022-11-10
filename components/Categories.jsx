092bab2fc596989153bff08d3ea89ea304bb07a6
jeffersondrs <jeffersonduarte@id.uff.br>
2022-11-10 01:11:28 -0300

    Updating blogs working with some posts

---
+++ components/Categories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Categories = ({post}) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 h-96">Categories</div>
+  );
+};
+
+export default Categories;
---
+++ components/PostWidget.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PostWidget = () => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 h-96"> Widgets</div>
+  );
+};
+
+export default PostWidget;

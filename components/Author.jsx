6864b70e32d27824ec0054c6d2df3742abd4ba2d
jeffersondrs <jeffersonduarte@id.uff.br>
2022-11-11 01:05:21 -0300

    Pages slugs and detail posts working

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const Author = () => {
+import React from "react";
+import Image from "next/image";
+const Author = ({ author }) => {
   return (
-    <div><h1>POST DETAIL</h1></div>
-  )
-}
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-40">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          className="align-middle rounded-full"
+          alt={author.name}
+          height="80"
+          width="80"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
+  );
+};
 
-export default Author
+export default Author;
